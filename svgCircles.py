184f3af9702f8ccb65eb612ffdb34cf22cdd404d
Jim Frederick <jimfred@jimfred.org>
2021-05-03 20:43:21 -0400

    cleanup. Add .svg file

--- svgCircles.py
+++ svgCircles.py
@@ -1,27 +1,31 @@
+import os.path
+
 import svgwrite
 import math
-
-units = svgwrite.inch
-dwg = svgwrite.Drawing(filename="0.svg", profile='tiny', height=12*units, width=12*units)
-
-dwg.viewbox(minx=-1200, miny=-1200, width=2*1200, height=2*1200)
-
-blue = svgwrite.rgb(0, 0, 255)
 
 circle_radial_qty = 80
 circle_layer_qty = 4
 circle_path_radius = 6
 
+units = svgwrite.inch
+svg_name = os.path.splitext(__file__)[0] + ".svg"  # output .svg file based on this file's name.
+
+dwg = svgwrite.Drawing(filename=svg_name, profile='tiny', height=circle_path_radius*2*units, width=circle_path_radius*2*units)
+
+dwg.viewbox(minx=-1200, miny=-1200, width=2*1200, height=2*1200)
+
+blue = svgwrite.rgb(0, 0, 255)
+
 angle_inc = 360 / circle_radial_qty
 r2 = math.sin(math.radians(angle_inc / 2)) * circle_path_radius
 
-for i in range(circle_radial_qty):
-    angle = angle_inc * i + 0
+for spoke in range(circle_radial_qty):
+    angle = angle_inc * spoke + 0
 
-    for j in range(circle_layer_qty):
-        x = math.cos(math.radians(angle)) * (circle_path_radius + j * 2 * r2)
-        y = math.sin(math.radians(angle)) * (circle_path_radius + j * 2 * r2)
-        circle = dwg.circle(center=(x * units, y * units), r=r2 * units, fill=blue, stroke=blue, stroke_width=0)
+    for layer in range(circle_layer_qty):
+        x = math.cos(math.radians(angle)) * (circle_path_radius + layer * 2 * r2)
+        y = math.sin(math.radians(angle)) * (circle_path_radius + layer * 2 * r2)
+        circle = dwg.circle(center=(x * units, y * units), r=r2 * units, fill=blue, stroke_width=0)
         dwg.add(circle)
 
 dwg.save()
