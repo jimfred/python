2de6416e78cc4ea921e13c2454c1e770175a6cab
JimFred <james.frederick@brukerdetection.us>
2021-09-23 08:56:20 -0400

    modify bat file to run setup.py

--- cython/CallWindowsApi/build.bat
+++ cython/CallWindowsApi/build.bat
@@ -1,4 +1,4 @@
-@call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+::(not needed) call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
 python setup.py build_ext --inplace
 
